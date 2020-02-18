515b308c29a0f43acc680a510981d1ea035d98bf
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-02-18 13:00:41 +0900

    Create wireframebeta.js

---
+++ wireframebeta.js
@@ -0,0 +1,218 @@
+//v0.5
+;(function(root){
+ ;
+ let layer=`
+ <g class="layer" display="none">
+  <title>Layer 1</title>
+ </g>
+ <g class="layer" display="none">
+  <title>total</title>
+ </g>
+ <g class="layer" display="inline">
+  <title>ground</title>
+  <line fill="none" fill-opacity="0" id="svg_9" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="140" x2="80" y1="140" y2="200"/>
+  <line fill="none" fill-opacity="0" id="svg_15" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="180" x2="240" y1="140" y2="200"/>
+  <line fill="none" fill-opacity="0" id="svg_16" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="110" x2="210" y1="170" y2="170"/>
+  <line fill="none" fill-opacity="0" id="svg_17" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="130" x2="190" y1="150" y2="150"/>
+  <line fill="none" fill-opacity="0" id="svg_19" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="140" x2="180" y1="140" y2="140"/>
+  <line fill="none" fill-opacity="0" id="svg_18" stroke="#191919" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" x1="160" x2="160" y1="140" y2="200"/>
+ </g>
+ <g class="layer" display="inline" id="D2">
+  <title>D2</title>
+  <path d="m180,100l10,-10l50,0c0,-2 0,62 0,60l-50,0c-2,-2 -10,-10 -10,-10l0,-40z" fill="#000000" id="svg_12" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+  <line fill="none" fill-opacity="0" id="svg_13" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" x1="190" x2="190" y1="90" y2="150"/>
+ </g>
+ <g class="layer" display="inline" id="D0">
+  <title>D0</title>
+  <path d="m140,100l-10,-10l-50,0l0,60l50,0c0,0 10,-10 10,-10l0,-40z" fill="#000000" id="svg_10" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+  <line fill="none" fill-opacity="0" id="svg_11" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" x1="130" x2="130" y1="90" y2="150"/>
+ </g>
+ <g class="layer" display="inline" id="D1">
+  <title>D1</title>
+  <rect fill="#000000" height="60" id="svg_14" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" width="60" x="130" y="90"/>
+ </g>
+ <g class="layer" display="none" id="D3">
+  <title>D3</title>
+  <path d="m130,150l-20,20l-30,0l0,-100l30,0c-2,-2 22,22 20,20l0,60z" fill="#000000" id="svg_20" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+  <line fill="none" id="svg_21" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" x1="110" x2="110" y1="70" y2="170"/>
+ </g>
+ <g class="layer" display="inline" id="D5">
+  <title>D5</title>
+  <path d="m240,70l-30,0l-20,20l0,60l20,20l30,0l0,-100z" fill="#000000" id="svg_23" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+  <line fill="none" id="svg_24" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" x1="210" x2="210" y1="70" y2="170"/>
+ </g>
+ <g class="layer" display="none" id="D4">
+  <title>D4</title>
+  <rect fill="#000000" height="100" id="svg_22" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" width="100" x="110" y="70"/>
+ </g>
+ <g class="layer" display="none" id="D4D">
+  <title>D4D</title>
+  <rect fill="#000000" height="100" id="svg_52" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" width="100" x="110" y="70"/>
+  <rect fill="#000000" height="80" id="svg_31" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" width="60" x="130" y="90"/>
+  <line fill="none" id="svg_32" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" x1="160" x2="160" y1="90" y2="170"/>
+ </g>
+ <g class="layer" display="none" id="D4SD">
+  <title>D4SD</title>
+  <path d="m125,165c0,0 10,-10 10,-10c0,0 50,0 50,0c0,0 10,10 10,10c0,0 -70,0 -70,0z" fill="#000000" id="svg_50" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none" id="D4SU">
+  <title>D4SU</title>
+  <path d="m125,75c0,0 10,10 10,10c0,0 50,0 50,0c0,0 10,-10 10,-10c0,0 -70,0 -70,0z" fill="#000000" id="svg_51" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none" id="D4O">
+  <title>D4O</title>
+  <path d="m160,160l-10,-20l10,-20l10,20l-10,20z" fill="#000000" id="svg_54" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none" id="D6">
+  <title>D6</title>
+  <path d="m110,70c2,2 -30,-30 -30,-30c0,0 0,160 0,160l30,-30l0,-100z" fill="#000000" id="svg_25" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none" id="D8">
+  <title>D8</title>
+  <path d="m210,70l30,-30c0,0 0,160 0,160l-30,-30l0,-100z" fill="#000000" id="svg_27" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none" id="D7">
+  <title>D7</title>
+  <rect fill="#000000" height="160" id="svg_28" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null" width="160" x="80" y="40"/>
+ </g>
+ <g class="layer" display="inline" id="D7O">
+  <title>D7O</title>
+  <path d="m160,185l-15,-35l15,-35l15,35l-15,35z" fill="#000000" id="svg_56" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="null"/>
+ </g>
+ <g class="layer" display="none">
+  <title>grid</title>
+  <rect fill="#000000" fill-opacity="0" height="160" id="svg_1" stroke="#333333" width="160" x="80" y="40"/>
+  <line fill="none" fill-opacity="0" id="svg_2" stroke="#333333" x1="80" x2="240" y1="120" y2="120"/>
+  <line fill="none" fill-opacity="0" id="svg_3" stroke="#333333" x1="160" x2="160" y1="40" y2="200"/>
+  <line fill="none" fill-opacity="0" id="svg_4" stroke="#333333" x1="80" x2="240" y1="40" y2="200"/>
+  <line fill="none" fill-opacity="0" id="svg_5" stroke="#333333" x1="240" x2="80" y1="40" y2="200"/>
+  <rect fill="none" fill-opacity="0" height="40" id="svg_6" stroke="#333333" width="40" x="140" y="100"/>
+  <rect fill="none" fill-opacity="0" height="100" id="svg_7" stroke="#333333" width="100" x="110" y="70"/>
+  <rect fill="none" fill-opacity="0" height="60" id="svg_8" stroke="#333333" width="60" x="130" y="90"/>
+ </g>
+ <g class="layer" display="inline">
+  <title>bigframe</title>
+  <rect fill="none" fill-opacity="0" height="160" id="svg_29" stroke="#000000" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="5" width="160" x="80" y="40"/>
+  <rect fill="none" fill-opacity="0" height="160" id="svg_57" rx="2" ry="2" stroke="#ffffff" stroke-dasharray="null" stroke-linecap="null" stroke-linejoin="null" stroke-width="2" width="160" x="80" y="40"/>
+ </g>
+`
+ ;
+ let fn={}; fn.q=(d)=>document.querySelector(d);
+ fn.gi=(d,el)=>document.getElementById(d)
+ let is={}; is.string = function(obj){return toString.call(obj) === '[object String]'}
+ ;
+ function setlayer(el){
+  el.innerHTML+=layer;
+ }
+ function drawwire(ary,el){
+  //console.log(ary,el)
+  let s='wall',v='none'
+  //
+  s=ary[0][0]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D0').style.display=v
+  //  
+  s=ary[0][1]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D1').style.display=v
+  //
+  s=ary[0][2]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D2').style.display=v  
+  //
+  //
+  s=ary[1][0]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D3').style.display=v  
+  s=ary[1][1]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D4').style.display=v
+  //
+  //D4D
+  //D4O
+  //D4SD
+  //D4SU  
+  if(s==='object'||s==='event') v='inline'
+  else v='none'  
+  fn.gi('D4O').style.display=v 
+  if(s==='door') v='inline'
+  else v='none'  
+  fn.gi('D4D').style.display=v 
+  if(s==='downstair') v='inline'
+  else v='none'  
+  fn.gi('D4SD').style.display=v
+  if(s==='upstair') v='inline'
+  else v='none'  
+  fn.gi('D4SU').style.display=v   
+  //
+  
+  s=ary[1][2]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D5').style.display=v  
+  //
+  s=ary[2][0]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D6').style.display=v  
+  s=ary[2][1]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D7').style.display=v
+  if(s==='object'||s==='event') v='inline'
+  else v='none'
+  fn.gi('D7O').style.display=v 
+  
+  s=ary[2][2]
+  if(s==='wall') v='inline'
+  else v='none'
+  fn.gi('D8').style.display=v
+  //
+
+  
+ }
+ ;
+ function entry(opt,query){
+  ;
+  let o={
+  '0':'wall'
+ ,'1':'road'
+ ,'2':'door'
+ ,'3':'object'
+ ,'4':'event'
+ ,'5':'downstair'
+ ,'6':'upstair'
+ //
+ ,'query':query||'svg'
+ ,'first':true
+ }
+  o.el=fn.q(o.query)
+  if(!o.el)return console.log('query error'),{}
+  ;
+  o.ef=(c,ms)=>{
+   ms=ms||30,c=c.replace('.',''),o.el.classList.add(c)   
+   return setTimeout(()=>{ o.el.classList.remove(c) },ms), o
+  }
+  ;
+  o.wire=(obj,c,ms)=>{
+   if(o.first)return setlayer(o.el),o.first=false,o.wire(obj,c,ms)
+   ;
+   let ary=is.string(obj)?obj.trim().split('\n').map(d=>d.split('')):obj
+   //ary[3][3] [['0','0','0'],...]
+   for(let y=0;y<3;y++)
+    for(let x=0;x<3;x++)
+     ary[y][x]=o[ary[y][x]]||'wall' //default wall
+   ;
+   if(c)o.ef(c,ms)
+   return drawwire(ary,o.el), o
+  }
+  ;
+  return Object.assign(o,opt)
+ }
+ root.wireframebeta=entry
+})(this);
